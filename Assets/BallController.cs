09b307dce098fd129dab9ef9d887da25269f9e0b
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-13 10:57:17 +0900

    PinBallプロジェクト　３Dゲーム作成
    課題提出

---
+++ Assets/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosz = -6.5f;
+
+    private GameObject gameoverText;
+
+	void Start ()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+	}
+	
+	void Update ()
+    {
+        if (this.transform.position.z < visiblePosz)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+	}
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//得点を表示しよう
+
+public class ScoreController : MonoBehaviour
+{
+    private GameObject scoreText;
+
+    private int score = 0;
+
+	void Start ()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            point(1);
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            point(5);
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            point(10);
+        }
+        else if (other.gameObject.tag == "LareCloudrTag")
+        {
+            point(30);
+        }
+    }
+
+    void point(int a)
+    {
+        score += a;
+        this.scoreText.GetComponent<Text>().text = "Score:" + score.ToString("D4");
+    }
+}
